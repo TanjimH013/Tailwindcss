8962417ea5c7063382d160d472676147d9c05c93
tanjim <tanjim13hasan@gmail.com>
2023-12-24 21:04:55 +0600

    LWS-DarkMode-Variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
+  purge:[],
+  darkMode: "media", // or "media" or "class"
   theme: {
+    extend: {},
+  },
+  variants:{
     extend: {},
   },
   plugins: [],
