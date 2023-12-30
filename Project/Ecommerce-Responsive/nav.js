073d50a3a944732bffe1f0a8f1454ae92aebf40e
tanjim <tanjim13hasan@gmail.com>
2023-12-30 15:21:12 +0600

    Project-Ecommerce-create-JS-file-link

---
+++ Project/Ecommerce-Responsive/nav.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const mobileNav = document.getElementById('mobile-nav');
+
+hamburger.addEventListener('click', ()=>{
+    mobileNav.classList.toggle('-translate-x-full')
+})
